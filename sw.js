59f87074952d59af17820bbb27edbb1c765e5d58
abdallahfarouk959-star <abdallahfarouk959@gmail.com>
2025-09-09 10:40:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+// Simple service worker for PWA install
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
